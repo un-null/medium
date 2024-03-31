3f0078b7a569c1d708af77861be00026bdca03b3
null <keita@gaogao.asia>
2024-04-01 00:53:34 +0900

    ✨ add heading_3 and bulleted list block

--- src/components/blocks/block-to-jsx.tsx
+++ src/components/blocks/block-to-jsx.tsx
@@ -1,5 +1,7 @@
 import type { BlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 import { Paragraph } from "./paragraph";
+import { Heading } from "./heading";
+import { BulletedList } from "./bulleted-list";
 
 export const BlockToJSX = ({ block }: { block: BlockObjectResponse }) => {
   const blockType = block.type;
@@ -7,6 +9,14 @@ export const BlockToJSX = ({ block }: { block: BlockObjectResponse }) => {
   switch (blockType) {
     case "paragraph":
       return <Paragraph block={block} />;
+    case "heading_3":
+      return <Heading block={block} />;
+    case "bulleted_list_item":
+      return (
+        <ul className="my-4">
+          <BulletedList block={block} />
+        </ul>
+      );
     default:
       return <></>;
   }
