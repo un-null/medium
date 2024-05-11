a9da4205b4a970a76fcfb2b663f145f31a247736
null <keita@gaogao.asia>
2024-05-11 21:20:01 +0900

    💄 fix home ui #13

--- src/lib/notion.ts
+++ src/lib/notion.ts
@@ -85,6 +85,50 @@ export const getArticles = async () => {
   return filterdArticleData;
 };
 
+export const getLatestArticles = async () => {
+  const db = import.meta.env.NOTION_ARTICLE_DATABASE_ID;
+
+  const results = (
+    await notion.databases.query({
+      database_id: db,
+      filter: {
+        property: "ステータス",
+        select: {
+          equals: "公開済",
+        },
+      },
+      page_size: 3,
+      sorts: [{ property: "公開日", direction: "descending" }],
+    })
+  ).results;
+
+  // Fix using reduce ???
+  let filterdArticleData: FilterdArticlesData = [];
+
+  results.map((d) => {
+    if ("properties" in d) {
+      const id = d.id;
+      const title =
+        d.properties["タイトル"]?.type === "title"
+          ? d.properties["タイトル"].title.map((t) => t.plain_text)
+          : [""];
+      if (
+        d.properties["編纂員"]?.type === "created_by" &&
+        "name" in d.properties["編纂員"].created_by
+      ) {
+        const user = {
+          name: d.properties["編纂員"].created_by.name || "",
+          avatar: d.properties["編纂員"].created_by.avatar_url || "",
+        };
+        filterdArticleData.push({ id, title, ...user });
+      }
+    }
+    return [];
+  });
+
+  return filterdArticleData;
+};
+
 export const getWorks = async () => {
   const db = import.meta.env.NOTION_WORK_DATABASE_ID;
 
