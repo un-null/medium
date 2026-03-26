72ff5b1ea7796c45b3de052496fc6371c9e5d2ba
un-null <kawabata.qppq@gmail.com>
2026-03-26 13:59:17 +0700

    ✨ display mdx at home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { getLatestArticles } from "@/lib/notion";
+import { getAllPostsMeta } from "@/lib/posts";
 import { Card, CardContent, CardFooter } from "@/shadcn/components/ui/card";
 import { ChevronRight } from "lucide-react";
 import Image from "next/image";
 
 export default async function Home() {
-	const latestArticles = await getLatestArticles();
+	const latestArticles = (await getAllPostsMeta()).slice(0, 3);
 
 	return (
 		<div className="flex flex-col gap-8 px-4">
@@ -67,20 +67,22 @@ export default async function Home() {
 
 				<div className="mt-6 grid grid-cols-2 gap-4 sm:grid-cols-3">
 					{latestArticles.map((article) => (
-						<Card key={article.id} className="p-0 aspect-square">
-							<a href={`/article/${article.id}`}>
+						<Card key={article.slug} className="p-0 aspect-square">
+							<a href={`/article/${article.slug}`}>
 								<CardContent className="grid aspect-[4/3] place-items-center text-zinc-600 dark:text-zinc-400 hover:text-foreground dark:hover:text-foreground  dark:bg-zinc-950 rounded-t-xl border-b">
 									<p className="mt-4 text-sm md:text-base">{article.title}</p>
 								</CardContent>
 								<CardFooter className="flex items-center justify-center space-x-2 text-sm text-zinc-500 p-0 mt-3 sm:mt-4 cursor-default">
 									<p className="text-xs sm:text-sm">編纂員: {article?.name}</p>
-									<Image
-										src={article?.avatar}
-										width={16}
-										height={16}
-										alt={article?.name}
-										className="rounded-full sm:w-5 sm:h-5"
-									/>
+									{article.avatar && (
+										<Image
+											src={article.avatar}
+											width={16}
+											height={16}
+											alt={article.name}
+											className="rounded-full sm:w-5 sm:h-5"
+										/>
+									)}
 								</CardFooter>
 							</a>
 						</Card>
