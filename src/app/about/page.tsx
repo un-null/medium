31815da299c895d6a0ed707be402ad32d0b2f881
un-null <kawabata.qppq@gmail.com>
2026-03-26 14:11:49 +0700

    ♻️

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,24 +1,8 @@
-import { BlockToJSX } from "@/components/blocks/block-to-jsx";
-import { getHome } from "@/lib/notion";
+import Introduction from "@/../content/introduction.mdx";
 import { MoveLeft } from "lucide-react";
-import type { Metadata } from "next";
 import Link from "next/link";
 
-export const metadata: Metadata = {
-	title: "About",
-	description:
-		"A private media platform that shares passion, with 'null' as the medium.",
-	openGraph: {
-		title: "About",
-		description:
-			"A private media platform that shares passion, with 'null' as the medium.",
-		siteName: "Medium",
-	},
-};
-
-export default async function About() {
-	const blocks = await getHome();
-
+export default function About() {
 	return (
 		<div>
 			<Link
@@ -28,13 +12,9 @@ export default async function About() {
 				<MoveLeft />
 			</Link>
 
-			<ul className="mx-auto px-4">
-				{blocks.map((block) => (
-					<div key={block.id}>
-						<BlockToJSX block={block} />
-					</div>
-				))}
-			</ul>
+			<div className="mx-auto px-4 text-sm sm:text-base">
+				<Introduction />
+			</div>
 		</div>
 	);
 }
--- src/components/blocks/link-card.tsx
+++
@@ -1,8 +0,0 @@
-import type { BookmarkBlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
-// import ogs from "open-graph-scraper";
-
-// const { result } = await ogs({ url: block.bookmark.url });
-
-export function LinkCard({ block }: { block: BookmarkBlockObjectResponse }) {
-	return <p>link card</p>;
-}
